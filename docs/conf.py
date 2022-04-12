b843ecca9cb3c939048d1707960651feb62cb746
Taraka Rama <tarak225>
2022-04-12 12:29:56 +0530

    files

--- docs/conf.py
+++ docs/conf.py
@@ -47,12 +47,7 @@ mathjax_path = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-
 # -- Intersphinx ---------------------------------------------------------------
 
 intersphinx_cache_limit = 10     # days to keep the cached inventories
-intersphinx_mapping = {
-       # 'sphinx':('http://sphinx.pocoo.org',None),
-        'python':('http://docs.python.org/3.2',None),
-    'matplotlib':('http://matplotlib.sourceforge.net',None),
-         'numpy':('http://docs.scipy.org/doc/numpy',None),
-}
+intersphinx_mapping = {'python': ('https://docs.python.org/3/objects.inv')}
 
 # -- bibtex configuration settings ---------------------------------------------------------
 
